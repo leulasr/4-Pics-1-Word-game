730ffb823200865479ad79fe6117ce82da14c69a
Nikkyblaq711 <95155950+NikkyBlaq711@users.noreply.github.com>
2022-01-06 04:07:06 +0100

    Update github starter.py

--- github starter.py
+++ github starter.py
@@ -1,2 +1,3 @@
 "let's gooo time to get started on this project"
 "everyone add anything to make sure we understand how to use github"
+"hello this is Michael, and I believe we can achieve this if we work together"
