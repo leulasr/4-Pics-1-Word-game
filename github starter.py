fca0ca779e25b4268fa428e2f8a12a7e6815c28b
davidfatomilola <97130074+davidfatomilola@users.noreply.github.com>
2022-01-06 04:19:44 +0100

    Create github starter.py

--- github starter.py
+++ github starter.py
@@ -1,2 +1,4 @@
 "let's gooo time to get started on this project"
 "everyone add anything to make sure we understand how to use github"
+"i am adding my own sentence here, pls confirm"
+"that is all i wanna add for now"
